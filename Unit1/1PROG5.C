e485eddd51a4cea310800e4bee44d612285f7817
Ashutosh Pandey <pandeyashutoshkumar596@gmail.com>
2026-09-02 02:46:41 +0530

    Add files via upload

---
+++ Unit1/1PROG5.C
@@ -0,0 +1,36 @@
+/* Enrollment No: 92500527170
+/* 5. Create an array of size 10, find the largest value from the array. */
+
+
+#include<stdio.h>
+
+void main()
+{
+          int a[10],i,large;
+
+          for(i=0;i<10;i++)
+          {
+              printf("\n Enter the number %d of A:",i+1);
+              scanf("%d",&a[i]);
+
+          }
+          large=a[0];
+          for(i=0;i<10;i++)
+          {
+
+              if(large<a[i])
+              {
+                  large=a[i];
+              }
+
+          }
+           printf("The largest number is %d",large);
+}
+
+
+
+
+
+
+
+
